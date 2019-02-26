fac6afe2b76eb83dda3d93c180d5e9f932d5aa26
Olivier <olivier.revelat@gmail.com>
2019-02-26 22:29:38 +0100

    latest changes ?

--- win-speak-remote/node-speak-remote.js
+++ win-speak-remote/node-speak-remote.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const querystring = require('qs');
+const request = require('request');
 
 const helper  = require('node-red-viseo-helper');
 
@@ -22,7 +24,19 @@ const input = (node, data, config) => {
     let options = RED.nodes.getNode(config.options);
 
     // lets start the remote process
-    var url = 'http://' + options.remoteip + ':' + options.remoteport + '/tts?' + querystring.en;
+    var url = 'http://' + options.remoteip + ':' + options.remoteport + '/speak?tts=' + querystring.escape(tts);
 
-    // todo
+    request(url, function(err, response, body) {
+        if (err) {
+            console.log('@@@error:' + err);
+        }
+        else if (response && response.statusCode) {
+            if (body.toUpperCase() === 'OK'.toUpperCase()) {
+                node.send(data);
+            }
+            else {
+                console.log("remote process returned code " + response.statusCode);
+            }
+        }
+    });
 }
